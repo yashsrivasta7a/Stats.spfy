2568e09acc95393c4d9508db47ba84397b97a787
Yash Srivastava <120123563+yashsrivasta7a@users.noreply.github.com>
2025-01-25 01:52:46 +0530

    vercel hoja bhai

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Home.css';
-const clientId = import.meta.env.VITE_CLIENT_ID || 'http://localhost:5173/callback';;
-const redirectUri =  import.meta.env.VITE_REDIRECT_URI;
+const clientId = "e2daf0389bed41ad9f0c57e6ae9cddc1";
+const redirectUri =  "https://statsspotify.vercel.app/callback" 
 const authUri = "https://accounts.spotify.com/authorize"
 const scope = "user-top-read";
 function Home() {
