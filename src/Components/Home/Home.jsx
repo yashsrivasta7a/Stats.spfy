61307118fabcd21e7c2d0dcf342f735ad6631b39
Yash Srivastava <120123563+yashsrivasta7a@users.noreply.github.com>
2025-01-25 01:30:49 +0530

    bug fix

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import './Home.css';
-const clientId = process.env.REACT_APP_CLIENT_ID; 
-const redirectUri = process.env.REACT_APP_REDIRECT_URI;
-const authUri = process.env.REACT_APP_AUTH_URI;
+const clientId = import.meta.env.VITE_CLIENT_ID;
+const redirectUri = "http://localhost:5173/callback";
+const authUri = "https://accounts.spotify.com/authorize"
 const scope = "user-top-read";
 function Home() {
   const handleClick = () => {
-   
+    
     window.location.href = `${authUri}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scope}&response_type=token&show_dialog=true`;
   };
 
